0cbc8f237e2c8a7046a22b662aa91577c26c5c3d
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2025-12-12 21:20:18 +0100

    Cef settings adjusted for performance

--- AeroSurf/App.xaml.cs
+++ AeroSurf/App.xaml.cs
@@ -14,9 +14,14 @@ namespace AeroSurf
 
             var settings = new CefSettings();
             settings.CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "AeroSurfCache");
-
+            settings.LogSeverity = LogSeverity.Disable;
+            settings.CefCommandLineArgs.Add("no-proxy-server");
+            settings.CefCommandLineArgs.Add("enable-quic");
             settings.CefCommandLineArgs.Add("enable-gpu");
+            settings.CefCommandLineArgs.Add("enable-gpu-rasterization");
+            settings.CefCommandLineArgs.Add("disable-gpu-vsync"); 
             settings.CefCommandLineArgs.Add("renderer-process-limit", "10");
+            settings.CefCommandLineArgs.Add("autoplay-policy", "no-user-gesture-required");
 
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
         }
